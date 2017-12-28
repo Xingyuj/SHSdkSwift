6fb76510e6ce81e910ff2cdd1e54b77b373f3765
EthanXingyuJi <endorathas@gmail.com>
2017-12-29 01:20:55 +1100

    add dependencies: Alamofire and SwiftyJSON

--- SHSdkSwift/SHSdkSwift.h
+++ SHSdkSwift/SHSdkSwift.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double SHSdkSwiftVersionNumber;
 FOUNDATION_EXPORT const unsigned char SHSdkSwiftVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SHSdkSwift/PublicHeader.h>
-
-
